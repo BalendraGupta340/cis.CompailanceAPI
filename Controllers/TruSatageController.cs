d701b92ce9210493b8ad968d9bc18f7d89665df4
USER <USER@DESKTOP-LIHDEMI>
2025-02-02 11:58:18 +0530

    AddChangesFile-CA-160

---
+++ Controllers/TruSatageController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TruStage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TruSatageController : ControllerBase
+    {
+    }
+}
